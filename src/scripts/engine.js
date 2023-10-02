5c741c06f0094e2580cfb0280bb0eb9ed61b9967
MarcosWinther <winthermarcos@gmail.com>
2023-10-02 13:24:32 -0300

    criado o efeito de rotate nos cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -20,9 +20,26 @@ let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for(let i = 0; i < emojis.length; i++) {
-   let box = document.createElement('div');
-   box.className = 'item';
-   box.innerHTML = shuffleEmojis[i];
-   document.querySelector('.game').appendChild(box);
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
+  document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
+
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  }
+
+  console.log(openCards);
+}
+
+function checkMatch() {
 }
